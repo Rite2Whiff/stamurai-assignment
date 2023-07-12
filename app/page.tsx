303dd95895e3104950bede4a05e3fe5fa1f5acfd
Mohit <msukhlani20@gmail.com>
2023-07-12 21:20:39 +0530

    setting up a basic structute and some functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,125 @@
-import Image from 'next/image'
+"use client";
+
+import { useState } from "react";
+import Task from "./Task";
+import { AiFillDelete } from "react-icons/ai";
+import { FiEdit2 } from "react-icons/fi";
 
 export default function Home() {
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [taskValues, setTaskValues] = useState({
+    name: " ",
+    description: " ",
+  });
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState<number | null>(null);
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (taskValues.name === " " || taskValues.description === " ") {
+      alert("Please enter a value");
+    } else if (taskValues.name && taskValues.description && isEditing) {
+      setTasks(
+        tasks.map((task) => {
+          if (task.id === editId) {
+            return {
+              ...task,
+              name: taskValues.name,
+              description: taskValues.description,
+            };
+          }
+          return task;
+        })
+      );
+      setEditId(null);
+      setIsEditing(false);
+      setTaskValues({ ...taskValues, name: " ", description: "" });
+    } else {
+      const newTask = {
+        id: Date.now(),
+        name: taskValues.name,
+        description: taskValues.description,
+      };
+      setTasks([...tasks, newTask]);
+      setTaskValues({ ...taskValues, name: " ", description: "" });
+    }
+  };
+
+  const removeTasks = (id: number) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id);
+    setTasks(updatedTasks);
+  };
+
+  const updateTasks = (id: number) => {
+    const currentItem = tasks.find((task) => task.id === id)!;
+    setEditId(id);
+    setIsEditing(true);
+    setTaskValues({
+      ...taskValues,
+      name: currentItem?.name,
+      description: currentItem?.description,
+    });
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main>
+      <h1>Task Management Application</h1>
+      <form onSubmit={handleSubmit}>
+        <label className="inline-block">
+          <span>Task Name</span>
+          <input
+            type="text"
+            name="text"
+            className=" mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
+            value={taskValues.name}
+            onChange={(e) =>
+              setTaskValues({ ...taskValues, name: e.target.value })
+            }
+          />
+        </label>
+        <label className="inline-block px-5">
+          <span>Task Description</span>
+          <input
+            type="text"
+            name="text"
+            className=" mt-1 px-8 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1 "
+            value={taskValues.description}
+            onChange={(e) =>
+              setTaskValues({ ...taskValues, description: e.target.value })
+            }
+          />
+        </label>
+        <div className="w-40 inline-block">
+          <label className="select inline-block text-sm font-medium text-gray-700">
+            Task Status
+          </label>
+          <select id="select" className="form-select py-2 ml-10 mt-1 ">
+            <option>To Do</option>
+            <option>In Progress</option>
+            <option>Completed</option>
+          </select>
         </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+        <button type="submit" className="mx-10">
+          {isEditing ? "Edit Task" : "Add Task"}
+        </button>
+      </form>
+      <section className="task-list">
+        {tasks.map((task) => {
+          const { id, name, description } = task;
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <p>{description}</p>
+              <button onClick={() => removeTasks(id)}>
+                <AiFillDelete />
+              </button>
+              <button onClick={() => updateTasks(id)}>
+                <FiEdit2 />
+              </button>
+            </div>
+          );
+        })}
+      </section>
     </main>
-  )
+  );
 }
